9bbaa3687f50e5a9add2a44c962a007baf9cd4a7
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-10-13 20:14:30 +0530

    clean

--- src/auth/HandleUserLevels.jsx
+++ src/auth/HandleUserLevels.jsx
@@ -1,9 +1,7 @@
 /* eslint-disable react/prop-types */
-import { Navigate, Outlet, useLocation } from 'react-router'
+import { Navigate, Outlet } from 'react-router'
 
 const HandleUserLevels = ({ userType }) => {
-  const currentRoute = useLocation()
-  console.log(currentRoute)
   return userType ? (
     <Outlet />
   ) : (
